igloo-project/bindgen

commit cd6bdfae523df1dd937541fda557ce5d0f776dbb

Tick to a non-ancient version of joist-util.

processor/src/main/java/org/bindgen/processor/generators/BindKeywordGenerator.java
@@ -137,7 +137,7 @@ public class BindKeywordGenerator {
 	private void writeBindKeywordClass() {
 		try {
 			this.queue.log("WRITING " + CLASS_NAME + ".java");
-			JavaFileObject jfo = getFiler().createSourceFile(this.bindClass.getFullClassNameWithoutGeneric());
+			JavaFileObject jfo = getFiler().createSourceFile(this.bindClass.getSimpleName());
 			Writer w = jfo.openWriter();
 			w.write(this.bindClass.toCode());
 			w.close();

processor/src/main/java/org/bindgen/processor/generators/BindingClassGenerator.java
@@ -157,12 +157,11 @@ public class BindingClassGenerator {
 
 	private void saveCode(GClass gc) {
 		try {
-			JavaFileObject jfo = getFiler()
-				.createSourceFile(gc.getFullClassNameWithoutGeneric(), Copy.array(Element.class, Copy.list(this.sourceElements)));
+			JavaFileObject jfo = getFiler().createSourceFile(gc.getSimpleName(), Copy.array(Element.class, Copy.list(this.sourceElements)));
 			Writer w = jfo.openWriter();
 			w.write(gc.toCode());
 			w.close();
-			this.queue.log("Saved " + gc.getFullClassNameWithoutGeneric());
+			this.queue.log("Saved " + gc.getSimpleName());
 		} catch (IOException io) {
 			getMessager().printMessage(Kind.ERROR, io.getMessage(), this.element);
 		}

processor/ivy.xml
@@ -15,7 +15,7 @@
 	</publications>
 	<dependencies>
 		<!-- joist.util is jarjar'd into bindgen.jar so we count it as a build dependency. -->
-		<dependency org="joist" name="joist-util" rev="2010063001" conf="build->default,sources"/>
+		<dependency org="joist" name="joist-util" rev="1.4.17" conf="build->default,sources"/>
 		<dependency org="com.tonicsystems" name="jarjar" rev="1.0" conf="build->default"/>
 		<dependency org="junit" name="junit" rev="4.7" conf="tests->default,sources"/>
 	</dependencies>

processor/src/main/java/org/bindgen/processor/generators/FieldPropertyGenerator.java
@@ -92,14 +92,14 @@ public class FieldPropertyGenerator implements PropertyGenerator {
 
 	private void addInnerClassGetParent() {
 		GMethod getParent = this.innerClass.getMethod("getParentBinding").returnType("Binding<?>").addAnnotation("@Override");
-		getParent.body.line("return {}.this;", this.outerClass.getSimpleClassNameWithoutGeneric());
+		getParent.body.line("return {}.this;", this.outerClass.getSimpleName());
 	}
 
 	private void addInnerClassGet() {
 		GMethod get = this.innerClass.getMethod("get").returnType(this.property.getSetType()).addAnnotation("@Override");
 		get.body.line("return {}{}.this.get().{};",//
 			this.property.getCastForReturnIfNeeded(),
-			this.outerClass.getSimpleClassNameWithoutGeneric(),
+			this.outerClass.getSimpleName(),
 			this.fieldName);
 	}
 
@@ -108,19 +108,19 @@ public class FieldPropertyGenerator implements PropertyGenerator {
 		getWithRoot.argument("R", "root").returnType(this.property.getSetType()).addAnnotation("@Override");
 		getWithRoot.body.line("return {}{}.this.getWithRoot(root).{};",//
 			this.property.getCastForReturnIfNeeded(),
-			this.outerClass.getSimpleClassNameWithoutGeneric(),
+			this.outerClass.getSimpleName(),
 			this.fieldName);
 	}
 
 	private void addInnerClassGetSafelyWithRoot() {
 		GMethod m = this.innerClass.getMethod("getSafelyWithRoot");
 		m.argument("R", "root").returnType(this.property.getSetType()).addAnnotation("@Override");
-		m.body.line("if ({}.this.getSafelyWithRoot(root) == null) {", this.outerClass.getSimpleClassNameWithoutGeneric());
+		m.body.line("if ({}.this.getSafelyWithRoot(root) == null) {", this.outerClass.getSimpleName());
 		m.body.line("    return null;");
 		m.body.line("} else {");
 		m.body.line("    return {}{}.this.getSafelyWithRoot(root).{};",//
 			this.property.getCastForReturnIfNeeded(),
-			this.outerClass.getSimpleClassNameWithoutGeneric(),
+			this.outerClass.getSimpleName(),
 			this.fieldName);
 		m.body.line("}");
 	}
@@ -133,7 +133,7 @@ public class FieldPropertyGenerator implements PropertyGenerator {
 			return;
 		}
 		set.body.line("{}.this.get().{} = {};",//
-			this.outerClass.getSimpleClassNameWithoutGeneric(),
+			this.outerClass.getSimpleName(),
 			this.fieldName,
 			this.property.getName());
 	}
@@ -146,7 +146,7 @@ public class FieldPropertyGenerator implements PropertyGenerator {
 			return;
 		}
 		setWithRoot.body.line("{}.this.getWithRoot(root).{} = {};",//
-			this.outerClass.getSimpleClassNameWithoutGeneric(),
+			this.outerClass.getSimpleName(),
 			this.fieldName,
 			this.property.getName());
 	}

processor/src/main/java/org/bindgen/processor/generators/MethodCallableGenerator.java
@@ -114,7 +114,7 @@ public class MethodCallableGenerator implements PropertyGenerator {
 		run.returnType(this.blockMethod.getReturnType().toString());
 		run.body.line("{}{}.this.get().{}({});",//
 			this.getReturnPrefixIfNeeded(),
-			this.outerClass.getSimpleClassNameWithoutGeneric(),
+			this.outerClass.getSimpleName(),
 			this.methodName,
 			this.getArguments());
 		this.addMethodParameters(run);

processor/src/main/java/org/bindgen/processor/generators/MethodPropertyGenerator.java
@@ -138,7 +138,7 @@ public class MethodPropertyGenerator implements PropertyGenerator {
 
 	private void addInnerClassParent() {
 		GMethod getParent = this.innerClass.getMethod("getParentBinding").returnType("Binding<?>").addAnnotation("@Override");
-		getParent.body.line("return {}.this;", this.outerClass.getSimpleClassNameWithoutGeneric());
+		getParent.body.line("return {}.this;", this.outerClass.getSimpleName());
 	}
 
 	private void addInnerClassGet() {
@@ -146,7 +146,7 @@ public class MethodPropertyGenerator implements PropertyGenerator {
 		get.returnType(this.property.getSetType()).addAnnotation("@Override");
 		get.body.line("return {}{}.this.get().{}();",//
 			this.property.getCastForReturnIfNeeded(),
-			this.outerClass.getSimpleClassNameWithoutGeneric(),
+			this.outerClass.getSimpleName(),
 			this.methodName);
 	}
 
@@ -155,19 +155,19 @@ public class MethodPropertyGenerator implements PropertyGenerator {
 		getWithRoot.argument("R", "root").returnType(this.property.getSetType()).addAnnotation("@Override");
 		getWithRoot.body.line("return {}{}.this.getWithRoot(root).{}();",//
 			this.property.getCastForReturnIfNeeded(),
-			this.outerClass.getSimpleClassNameWithoutGeneric(),
+			this.outerClass.getSimpleName(),
 			this.methodName);
 	}
 
 	private void addInnerClassGetSafelyWithRoot() {
 		GMethod m = this.innerClass.getMethod("getSafelyWithRoot");
 		m.argument("R", "root").returnType(this.property.getSetType()).addAnnotation("@Override");
-		m.body.line("if ({}.this.getSafelyWithRoot(root) == null) {", this.outerClass.getSimpleClassNameWithoutGeneric());
+		m.body.line("if ({}.this.getSafelyWithRoot(root) == null) {", this.outerClass.getSimpleName());
 		m.body.line("    return null;");
 		m.body.line("} else {");
 		m.body.line("    return {}{}.this.getWithRoot(root).{}();",//
 			this.property.getCastForReturnIfNeeded(),
-			this.outerClass.getSimpleClassNameWithoutGeneric(),
+			this.outerClass.getSimpleName(),
 			this.methodName);
 		m.body.line("}");
 	}
@@ -193,7 +193,7 @@ public class MethodPropertyGenerator implements PropertyGenerator {
 		GMethod set = this.innerClass.getMethod("set({} {})", this.property.getSetType(), this.property.getName());
 		set.addAnnotation("@Override");
 		set.body.line("{}.this.get().{}({});",//
-			this.outerClass.getSimpleClassNameWithoutGeneric(),
+			this.outerClass.getSimpleName(),
 			this.prefix.setterName(this.methodName),
 			this.property.getName());
 	}
@@ -202,7 +202,7 @@ public class MethodPropertyGenerator implements PropertyGenerator {
 		GMethod setWithRoot = this.innerClass.getMethod("setWithRoot(R root, {} {})", this.property.getSetType(), this.property.getName());
 		setWithRoot.addAnnotation("@Override");
 		setWithRoot.body.line("{}.this.getWithRoot(root).{}({});",//
-			this.outerClass.getSimpleClassNameWithoutGeneric(),
+			this.outerClass.getSimpleName(),
 			this.prefix.setterName(this.methodName),
 			this.property.getName());
 	}

commit 0bc474531d1ebd5d8d5829e3ad5d9a16567061d9

Fixed bug (introduced in cd6bdfae523df1dd937541fda557ce5d0f776dbb?) that caused all binding files to be written in the default package.

processor/src/main/java/org/bindgen/processor/generators/BindKeywordGenerator.java
@@ -137,7 +137,7 @@ public class BindKeywordGenerator {
 	private void writeBindKeywordClass() {
 		try {
 			this.queue.log("WRITING " + CLASS_NAME + ".java");
-			JavaFileObject jfo = getFiler().createSourceFile(this.bindClass.getSimpleName());
+			JavaFileObject jfo = getFiler().createSourceFile(this.bindClass.getFullName());
 			Writer w = jfo.openWriter();
 			w.write(this.bindClass.toCode());
 			w.close();

processor/src/main/java/org/bindgen/processor/generators/BindingClassGenerator.java
@@ -157,11 +157,11 @@ public class BindingClassGenerator {
 
 	private void saveCode(GClass gc) {
 		try {
-			JavaFileObject jfo = getFiler().createSourceFile(gc.getSimpleName(), Copy.array(Element.class, Copy.list(this.sourceElements)));
+			JavaFileObject jfo = getFiler().createSourceFile(gc.getFullName(), Copy.array(Element.class, Copy.list(this.sourceElements)));
 			Writer w = jfo.openWriter();
 			w.write(gc.toCode());
 			w.close();
-			this.queue.log("Saved " + gc.getSimpleName());
+			this.queue.log("Saved " + gc.getFullName());
 		} catch (IOException io) {
 			getMessager().printMessage(Kind.ERROR, io.getMessage(), this.element);
 		} catch (NullPointerException npe) {