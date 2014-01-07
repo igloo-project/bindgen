66ca7252dbbdc85742f5aee7f306feaa5b87b3ce
David Shepherdson <david.shepherdson@precedencehealthcare.com>
2014-01-07 12:30:36 +1100

    Removed commented-out code from one of my earlier commits, since it isn't used and I can always get it back from the history if I find I need it later. :-)

--- processor/src/main/java/org/bindgen/processor/util/BoundProperty.java
+++ processor/src/main/java/org/bindgen/processor/util/BoundProperty.java
@@ -32,8 +32,6 @@ public class BoundProperty {
 	private final boolean isArray;
 	private ClassName name;
 
-	//private final TypeMirror typeInContext;
-
 	/**
 	 * @param enclosed the parent method or field
 	 * @param type our type
@@ -44,7 +42,6 @@ public class BoundProperty {
 		this.propertyName = propertyName;
 
 		type = Util.resolveTypeVarIfPossible(getTypeUtils(), outerElement, type).type;
-		//this.typeInContext = CurrentEnv.getTypeUtils().asMemberOf((DeclaredType) outerElement.asType(), ((DeclaredType) type).asElement());
 		this.isArray = type.getKind() == TypeKind.ARRAY;
 		// if we're an array, keep the primitive type, e.g. char[]
 		this.type = this.isArray ? type : Util.boxIfNeeded(type);
