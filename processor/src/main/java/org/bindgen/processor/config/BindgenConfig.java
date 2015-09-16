2e393b0d471d1a8a422b31c250f3cf3580432ee0
Guillaume Smet <guillaume.smet@gmail.com>
2015-09-16 15:25:18 +0200

    Add an option to exclude packages from the Bindgen scope
    By default, it excludes java.util.stream as Bindgen is unable to
    generate the bindings properly for this package

--- processor/src/main/java/org/bindgen/processor/config/BindgenConfig.java
+++ processor/src/main/java/org/bindgen/processor/config/BindgenConfig.java
@@ -1,6 +1,6 @@
 package org.bindgen.processor.config;
 
-import static org.bindgen.processor.CurrentEnv.getConfig;
+import static org.bindgen.processor.CurrentEnv.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -17,23 +17,26 @@ import org.bindgen.processor.util.Util;
 
 /**
  * Bindgen configuration.
- * 
- * This class encapsulates all code that is responsible for making
- * decisions that can be affected by configuration options.
- * 
+ *
+ * This class encapsulates all code that is responsible for making decisions
+ * that can be affected by configuration options.
+ *
  * @author igor.vaynberg
  */
 public class BindgenConfig {
 
 	private static final String SCOPE_PARAM = "scope";
+	private static final String EXCLUDE_PARAM = "exclude";
 	private final Map<String, String> options = new HashMap<String, String>();
 	private final Scope<ClassName> bindingScope;
+	private final Scope<ClassName> exclude;
 
 	public BindgenConfig(ProcessingEnvironment env) {
 		this.loadDefaultOptions();
 		this.loadAptKeyValueOptions(env);
 		this.loadBindgenDotProperties(env);
 		this.bindingScope = this.getBindingScope();
+		this.exclude = this.getBindingExclude();
 	}
 
 	public boolean shouldGenerateBindingFor(TypeElement type) {
@@ -41,18 +44,22 @@ public class BindgenConfig {
 	}
 
 	public boolean shouldGenerateBindingFor(ClassName name) {
-		return this.bindingScope.includes(name);
+		return this.bindingScope.includes(name) && !this.exclude.includes(name);
 	}
 
 	/**
-	 * Check if a type binding already exists. Allow to use bindings from dependencies.
-	 * 
-	 * @param name - type name to search
-	 * @param element - enclosing element
+	 * Check if a type binding already exists. Allow to use bindings from
+	 * dependencies.
+	 *
+	 * @param name
+	 *            - type name to search
+	 * @param element
+	 *            - enclosing element
 	 * @return true if a ...BindingPath already exists
 	 */
 	public boolean existsFieldTypeBindingFor(ClassName name, Element element) {
-		String bindingPathClassName = Util.lowerCaseOuterClassNames(element, getConfig().baseNameForBinding(name) + "BindingPath");
+		String bindingPathClassName = Util.lowerCaseOuterClassNames(element, getConfig().baseNameForBinding(name)
+				+ "BindingPath");
 		try {
 			Class.forName(bindingPathClassName);
 			return true;
@@ -76,7 +83,8 @@ public class BindgenConfig {
 		if (explicit != null) {
 			return "true".equals(explicit);
 		}
-		// javac doesn't like skipping existing bindings , so default to true if in javac
+		// javac doesn't like skipping existing bindings , so default to true if
+		// in javac
 		return CurrentEnv.get().getClass().getName().startsWith("com.sun");
 	}
 
@@ -92,12 +100,18 @@ public class BindgenConfig {
 		}
 	}
 
-	/** @return the fully qualified name of the super class of all Bindings - useful for integration */
+	/**
+	 * @return the fully qualified name of the super class of all Bindings -
+	 *         useful for integration
+	 */
 	public String bindingPathSuperClassName() {
 		return this.options.get("bindingPathSuperClass");
 	}
 
-	/** @return a list of class names to match void methods against for callable bindings */
+	/**
+	 * @return a list of class names to match void methods against for callable
+	 *         bindings
+	 */
 	public String[] blockTypesToAttempt() {
 		String attempts = this.options.get("blockTypes");
 		if (attempts == null) {
@@ -108,12 +122,18 @@ public class BindgenConfig {
 		return attempts.split(",");
 	}
 
-	/** @return whether the {@code @Generated} annotations should be added to the source output */
+	/**
+	 * @return whether the {@code @Generated} annotations should be added to the
+	 *         source output
+	 */
 	public boolean skipGeneratedTimestamps() {
 		return this.isEnabled("skipGeneratedTimestamps");
 	}
 
-	/** @return whether the field/method {@code name} of {@code element} should be skipped */
+	/**
+	 * @return whether the field/method {@code name} of {@code element} should
+	 *         be skipped
+	 */
 	public boolean skipAttribute(Element element, String name) {
 		return this.isEnabled("skipAttribute." + element.toString() + "." + name);
 	}
@@ -136,7 +156,20 @@ public class BindgenConfig {
 		}
 	}
 
-	// Default properties--this is ugly, but I could not get a bindgen.properties to be found on the classpath
+	private Scope<ClassName> getBindingExclude() {
+		String scopeExpression = this.options.get(EXCLUDE_PARAM);
+
+		if (scopeExpression == null) {
+			scopeExpression = "java.util.stream";
+		} else {
+			scopeExpression += ",java.util.stream";
+		}
+
+		return new PackageExpressionScope(scopeExpression);
+	}
+
+	// Default properties--this is ugly, but I could not get a
+	// bindgen.properties to be found on the classpath
 	private void loadDefaultOptions() {
 		this.options.put("fixRawType.javax.servlet.ServletConfig.initParameterNames", "String");
 		this.options.put("fixRawType.javax.servlet.ServletContext.attributeNames", "String");
