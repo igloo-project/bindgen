b389cb5ece0b86f74cb6959ade2e380233279a6c
Laurent Almeras <laurent.almeras@openwide.fr>
2017-01-05 18:23:40 +0100

    wip

--- processor/src/main/java/org/bindgen/processor/GenerationQueue.java
+++ processor/src/main/java/org/bindgen/processor/GenerationQueue.java
@@ -18,14 +18,17 @@ import org.bindgen.processor.generators.BindingClassGenerator;
 import org.bindgen.processor.util.BoundClass;
 import org.bindgen.processor.util.ClassName;
 
-/** Keeps a recursive list of TypeElements to generate bindings for.
+/**
+ * Keeps a recursive list of TypeElements to generate bindings for.
  *
- * As each binding is generated, its properties may need their
- * own binding classes generated, so they get put in this queue.
+ * As each binding is generated, its properties may need their own binding
+ * classes generated, so they get put in this queue.
  */
 public class GenerationQueue {
 
-	// Both Eclipse and javac will use the same processor instance for each compilation run (e.g. across all of the rounds), so this should be cumulative
+	// Both Eclipse and javac will use the same processor instance for each
+	// compilation run (e.g. across all of the rounds), so this should be
+	// cumulative
 	private final Set<String> written = new HashSet<String>();
 	// Any TypeElements waiting to have bindings generated
 	private final List<TypeElement> queue = new ArrayList<TypeElement>();
@@ -42,7 +45,10 @@ public class GenerationQueue {
 		this.skipBindKeyword = getConfig().skipBindKeyword();
 	}
 
-	/** Enqueue <code>element</code> even if it was written during a previous compilation run. */
+	/**
+	 * Enqueue <code>element</code> even if it was written during a previous
+	 * compilation run.
+	 */
 	public void enqueueForcefully(TypeElement element) {
 		// Even when done forcefully, we can only touch elements once/round
 		if (this.hasAlreadyBeenWrittenByThisCompilationRun(element)) {
@@ -51,9 +57,14 @@ public class GenerationQueue {
 		this.enqueue(element);
 	}
 
-	/** Enqueue <code>element</code> only if we haven't seen it either this compilation run or during a previous compilation run. */
+	/**
+	 * Enqueue <code>element</code> only if we haven't seen it either this
+	 * compilation run or during a previous compilation run.
+	 */
 	public void enqueueIfNew(TypeElement element) {
-		if (this.hasAlreadyBeenWrittenByThisCompilationRun(element) || this.hasAlreadyBeenWrittenByAPreviousCompilationRun(element)) {
+		if (this.hasAlreadyBeenWrittenByThisCompilationRun(element)
+				|| this.hasAlreadyBeenWrittenByAPreviousCompilationRun(element) || getConfig()
+						.existsFieldTypeBindingFor(new ClassName(element.getQualifiedName().toString()), element)) {
 			return;
 		}
 		this.enqueue(element);
@@ -74,7 +85,10 @@ public class GenerationQueue {
 		new BindKeywordGenerator(this).generate(this.written);
 	}
 
-	/** Outputs <code>message</code> to System.out, mostly useful for Debug As Eclipse/javac debugging. */
+	/**
+	 * Outputs <code>message</code> to System.out, mostly useful for Debug As
+	 * Eclipse/javac debugging.
+	 */
 	public void log(String message) {
 		if (this.logEnabled) {
 			System.out.println(message + " in " + this);
@@ -89,9 +103,10 @@ public class GenerationQueue {
 	/**
 	 * Whether this compilation run has already seen <code>element</code>.
 	 *
-	 * We recursively walk into bindings, so first check our in-memory set of what we've
-	 * seen so far this compilation run. Eclipse creates a new processor instance every time
-	 * the user hits save, so this only has things from this compilation run.
+	 * We recursively walk into bindings, so first check our in-memory set of
+	 * what we've seen so far this compilation run. Eclipse creates a new
+	 * processor instance every time the user hits save, so this only has things
+	 * from this compilation run.
 	 */
 	private boolean hasAlreadyBeenWrittenByThisCompilationRun(TypeElement element) {
 		return this.written.contains(element.toString());
@@ -99,12 +114,13 @@ public class GenerationQueue {
 
 	/**
 	 * Whether a previous compilation run has already see <code>element</code>
-	 * 
-	 * If we haven't seen it this compilation run, see if we've already output awhile ago (e.g.
-	 * last time they hit save), in which case we can skip it. If we really needed to do this
-	 * element again, Eclipse would have deleted our last output and this check wouldn't find
-	 * anything. But this does not work for javac, so we don't do it if told not to. Would be
-	 * nice to auto-detect javac.
+	 *
+	 * If we haven't seen it this compilation run, see if we've already output
+	 * awhile ago (e.g. last time they hit save), in which case we can skip it.
+	 * If we really needed to do this element again, Eclipse would have deleted
+	 * our last output and this check wouldn't find anything. But this does not
+	 * work for javac, so we don't do it if told not to. Would be nice to
+	 * auto-detect javac.
 	 */
 	private boolean hasAlreadyBeenWrittenByAPreviousCompilationRun(TypeElement element) {
 		if (this.skipExistingBindingCheck) {
@@ -112,10 +128,8 @@ public class GenerationQueue {
 		}
 		try {
 			ClassName bindingClassName = new BoundClass(element).getBindingClassName();
-			FileObject fo = getFiler().getResource(
-				StandardLocation.SOURCE_OUTPUT,
-				bindingClassName.getPackageName(),
-				bindingClassName.getSimpleName() + ".java");
+			FileObject fo = getFiler().getResource(StandardLocation.SOURCE_OUTPUT, bindingClassName.getPackageName(),
+					bindingClassName.getSimpleName() + ".java");
 			return fo.getLastModified() > 0; // exists already
 		} catch (IOException io) {
 			getMessager().printMessage(Kind.ERROR, io.getMessage(), element);
